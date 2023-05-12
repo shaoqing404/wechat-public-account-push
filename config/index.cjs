7110bca4ec87ec4a79c70db62f7499b6176c2f89
shaoqing404 <121151546+shaoqing404@users.noreply.github.com>
2023-05-12 10:13:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,6 +7,7 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-plus'
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -19,24 +20,24 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '陛下',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'd356610af4f44d629196c695edcba8ee',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-4',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '陛下', year: '1998', date: '11-13',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '998', date: '12-24',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -51,13 +52,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wx4671560c1711c600',
     }
   ],
 
