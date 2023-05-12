328cf6aa28198dffddaca1335b52eb60ba6d157b
shaoqing404 <121151546+shaoqing404@users.noreply.github.com>
2023-05-12 10:35:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,22 +7,25 @@
  */
 const USER_CONFIG = {
 
-  USE_PASSAGE: 'push-plus',
+//   USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '上海',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '陛下',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'd356610af4f44d629196c695edcba8ee',
+      //for wechat-test-push
+      id:'ocEFZ61vKSPp2mC_4oHtucQyE4Tw',
+//       for pushplus-push
+//       id: 'd356610af4f44d629196c695edcba8ee',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -34,7 +37,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '998', date: '12-24',
+          type: '生日', name: '晶宇', year: '998', date: '12-24',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '09-03',
